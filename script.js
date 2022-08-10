33618bf0fc9bef1314f260949d48ee8ce81e3c0b
AlleluaAmen <87348628+AlleluaAmen@users.noreply.github.com>
2022-08-10 21:18:20 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,21 @@
+function fb_add() {
+  const fb_address = window.open(
+    "https://www.facebook.com/profile.php?id=100083988625824",
+    "MsgWindow"
+  );
+}
+function tw_add() {
+  const tw_address = window.open("https://twitter.com/dagxnson_0", "MsgWindow");
+}
+function gh_add() {
+  const gh_address = window.open("https://github.com/dagxnson0", "MsgWindow");
+}
+function rd_add() {
+  const rd_address = window.open(
+    "https://www.reddit.com/user/DagXnSon0",
+    "MsgWindow"
+  );
+}
+function tt_add() {
+  const tt_address = window.open("https://tiktok.com", "MsgWindow");
+}
